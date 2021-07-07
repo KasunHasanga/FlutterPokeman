4882c48c4728ce88b0427952c09a234e3717e29f
KasunHasanga <kasunhasanga96@gmail.com>
2021-07-07 23:23:36 +0530

    fetch pokeman details

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'dart:convert';
+
+import 'package:Flutter/pokeman.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +16,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.cyan,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
@@ -29,15 +31,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String url =
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
 
-  String url ="https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
   @override
   void initState() {
     super.initState();
+    fetchData();
   }
 
-  fetchData()async{
+  PokeHub pokehub;
 
+  fetchData() async {
+    var res = await http.get(url);
+    var decodedJson = jsonDecode(res.body);
+    pokehub = PokeHub.fromJson(decodedJson);
+    setState(() {
+
+    });
   }
 
   @override
@@ -45,12 +56,45 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Poke App"),
-      ),floatingActionButton: FloatingActionButton(
-      onPressed: (){
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.refresh),
+      ),
+      body: (pokehub != null)
+          ? GridView.count(
+              crossAxisCount: 2,
+              children: pokehub.pokemon
+                  .map((poke) => Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Card(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                            Container(
+                              height: 100,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    poke.img
+                                  )
+                                )
+                              ),
+                            ),
+                            Text(poke.name,
 
-      },child: Icon(Icons.refresh),
-    )
-      ,body: Container(),
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold
+                              ),
+                            )
+                          ],),
+                        ),
+                      ))
+                  .toList(),
+            )
+          : Center(child: CircularProgressIndicator()),
     );
   }
 }
